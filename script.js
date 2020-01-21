5f6ea3343bf593452a97a01942872a5a31b042a7
hollenmatthewr <59899287+hollenmatthewr@users.noreply.github.com>
2020-01-21 18:51:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("what is your name?");
+var color = prompt("what is your favorite color?");
+
+document.body.innerHTML = "My name is" + " " + name  + " " + "and my favorite color is" + " " + color;
